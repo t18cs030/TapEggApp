d29770185da778a83e124cabfded532aa40c68d4
Kenta Tanii <t18cs030.y@gmail.com>
2021-01-11 12:45:32 +0900

    [add] Egg image transition

--- TapEggApp/CountView.swift
+++ TapEggApp/CountView.swift
@@ -9,6 +9,26 @@ import SwiftUI
 
 struct CountView: View {
     @ObservedObject var num = saveNumber()
+    func updateImage(number:Int) -> Image{
+        if(number == 100000)
+        {return Image("Broken-Egg-0")}
+        else if(99990<=number && number<100000)
+        {return Image("Broken-Egg-1")}
+        else if(99980<=number && number<99990)
+        {return Image("Broken-Egg-2")}
+        else if(99970<=number && number<99980)
+        {return Image("Broken-Egg-3")}
+        else if(99960<=number && number<99970)
+        {return Image("Broken-Egg-4")}
+        else if(99950<=number && number<99960)
+        {return Image("Broken-Egg-5")}
+        else if(99940<=number && number<99950)
+        {return Image("Broken-Egg-6")}
+        else if(99930<=number && number<99940)
+        {return Image("Broken-Egg-fin")}
+        else
+        {return Image("Broken-Egg-0")}
+    }
     var body: some View {
         ZStack{
             Image("BamBoo")
@@ -37,7 +57,8 @@ struct CountView: View {
                     Button(action:
                             {num.num -= 1;},
                            label:
-                            {Image("Broken-Egg-0")
+                            {
+                                updateImage(number:num.num)
                                 .resizable()
                                 .scaledToFit()
                                 .frame(width: 200,
