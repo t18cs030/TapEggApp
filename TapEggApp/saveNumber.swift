b72adc39771312927f590695757c11043f3b2677
Kenta Tanii <t18cs030.y@gmail.com>
2021-01-11 16:31:27 +0900

    [add]終了した回数を保存する変数を追加

--- TapEggApp/saveNumber.swift
+++ TapEggApp/saveNumber.swift
@@ -14,11 +14,25 @@ class saveNumber: ObservableObject{
             UserDefaults.standard.set(num,forKey: "num")
         }
     }
+    @Published var finishCount: Int{
+        didSet{
+            UserDefaults.standard.set(finishCount,forKey: "finishCount")
+        }
+    }
     init() {
-        UserDefaults.standard.register(defaults: ["num":100000])
+        UserDefaults.standard.register(defaults: ["num":100000,"finishCount":0])
         num = UserDefaults.standard.integer(forKey: "num")
+        finishCount = UserDefaults.standard.integer(forKey: "finishCount")
     }
     public func reset(){
         num = 100000
+        finishCount = 0
+    }
+    public func isFinish() -> Bool{
+        if(num == 0){return true}
+        else{return false}
+    }
+    public func addFinishCount(){
+        finishCount += 1;
     }
 }
